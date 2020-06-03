2b2241ab18c538be48a420dd23270eee0a6f0694
louis cai <1290103192@qq.com>
2020-06-03 14:49:52 +0800

    Daily routine/2020.6.3

---
+++ Algorithm_0501-1000/id0837_New21Game/code.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+double new21Game(int N, int K, int W)
+{
+    if (K == 0)
+    {
+        return 1.0;
+    }
+    vector<double> dp(K + W);
+    for (int i = K; i <= N && i < K + W; i++)
+    {
+        dp[i] = 1.0;
+    }
+    dp[K - 1] = 1.0 * min(N - K + 1, W) / W;
+    for (int i = K - 2; i >= 0; i--)
+    {
+        dp[i] = dp[i + 1] - (dp[i + W + 1] - dp[i + 1]) / W;
+    }
+    return dp[0];
+}
+
+int main()
+{
+    int N = 10, K = 1, W = 10;
+
+    cout << new21Game(N, K, W) << endl;
+}
